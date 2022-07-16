0a5b141f20cc2250449c3a2c3288d1cd8c7960e0
星野龍 <ryu0114h@gmail.com>
2022-07-16 16:33:08 +0900

    UI完成

---
+++ src/components/CellItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="cell" v-if="value === 1">◯</span>
+  <span class="cell" v-else-if="value === -1">✖️</span>
+  <span class="cell" @click="click" v-else></span>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "CellItem",
+  props: {
+    index: {
+      type: Number,
+    },
+    value: {
+      type: Number,
+    },
+  },
+  methods: {
+    click: function () {
+      this.$emit("click", this.index);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.cell {
+  border-radius: 10px;
+  border: 3px solid gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 64px;
+}
+
+.cell:hover {
+  background-color: aliceblue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <CellTable />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
@@ -7,12 +8,14 @@
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import CellTable from "@/components/CellTable.vue";
 
 export default Vue.extend({
   name: "HomeView",
   components: {
     HelloWorld,
+    CellTable,
   },
 });
 </script>
